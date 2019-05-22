e9deac2ecf6aae4bd05bc08e1d60d0f30487d8bf
Pavel Shukhman <taleodor@gmail.com>
2019-05-22 09:39:45 -0400

    Fix junit import in tests

--- src/test/java/io/reliza/versioning/AppTest.java
+++ src/test/java/io/reliza/versioning/AppTest.java
@@ -5,12 +5,13 @@
 
 package io.reliza.versioning;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import io.reliza.versioning.Version;
 import io.reliza.versioning.VersionApi;
